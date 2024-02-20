9a096fbcfd82765425d2b1ee43e5b721b4b33ce3
IKHSYAN-23 <mochamadikhsyankamil@smkwikrama.sch.id>
2024-02-20 09:59:20 +0700

    first-commit

---
+++ .php
@@ -0,0 +1,14 @@
+<?php
+$nilai=56;
+ 
+
+if($nilai >=80){
+    echo $nilai." di nytakan lulus";
+}else if($nilai >=68){
+    echo $nilai." di nyatakan lulus";
+}else if($nilai >=56){
+    echo $nilai." di nyatakan lulus";
+}else if($nilai >=45){
+    echo $nilai." tidak lulus";
+}else($nilai." tidak lulus")
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    $nama='Mochamad Ikhsyan Kamil';
+    $umur = 15;
+    echo "Nama saya adalah", $nama;
+    echo "<br>";
+    echo "Saya berumur $umur tahun";
+    ?>
